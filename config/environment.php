fd3344bdb84a5a31987ff2e24eed2263fa11f304
Unknown <devsbrasil@gmail.com>
2017-11-26 22:44:16 -0200

    First Commit

---
+++ config/environment.php
@@ -0,0 +1,9 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define environment type
+/*----------------------------------------------------*/
+return [
+    'local' => ['IGNISCODE-3.local'],
+    'production' => 'INSERT-HOSTNAME'
+];
